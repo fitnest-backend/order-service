1d693d7de11ef6b62ee1fe5b901778ac8a703dd3
Kamal Pashayev <infokamalpashayev@gmail.com>
2026-02-23 09:33:52 +0400

    marketplace -> catalog

--- src/main/java/az/fitnest/order/subscription/adapter/persistence/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package az.fitnest.order.subscription.adapter.persistence;
-
-import az.fitnest.order.subscription.domain.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Order, String> {
-}
